2f97041336ce4af4f5270a18a96b64cbf0dc87b2
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-22 11:27:24 -0300

    Add font-awesome and loading

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -10,6 +10,7 @@ import CompareList from '../../components/CompareList'
 
 export default class Main extends Component {
   state = {
+    loading: false,
     repositoryInput: '',
     repositories: [],
     repositoryError: false,
@@ -19,6 +20,8 @@ export default class Main extends Component {
     const { repositoryInput } = this.state
     e.preventDefault()
 
+    this.setState({ loading: true })
+
     try {
       const { data: repository } = await api.get(`/repos/${repositoryInput}`)
 
@@ -27,15 +30,18 @@ export default class Main extends Component {
       this.setState({
         repositoryInput: '',
         repositories: [...this.state.repositories, repository],
-        repositoryError: false
+        repositoryError: false,
       })
     } catch(err) {
       this.setState({ repositoryError: true })
+    } finally {
+      this.setState({ loading: false })
     }
   }
 
   render() {
     const {
+      loading,
       repositories,
       repositoryInput,
       repositoryError
@@ -51,7 +57,9 @@ export default class Main extends Component {
            placeholder='user/repository'
            value={repositoryInput}
            onChange={ e => this.setState({ repositoryInput: e.target.value })} />
-          <button type='submit'>ok</button>
+          <button type='submit'>
+            { loading ? <i className='fa fa-spinner fa-pulse' /> : 'ok' }
+          </button>
         </Form>
 
         < CompareList repositories={repositories} />
