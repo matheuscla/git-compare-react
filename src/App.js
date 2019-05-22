a994a50800836d2c34add219c69f6d143c7a1981
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-22 10:14:36 -0300

    Add repository form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Fragment } from 'react'
 
 import GlobalStyle from './styles/global'
 
+import Main from './pages/Main'
+
 const App = () => {
   return ( 
     <Fragment>
       <GlobalStyle />
+      <Main />
     </Fragment>
   )
 }
