7403bcc70e6e56d785405e59425bdf5f66af11c4
Matheus Lopes <matheuscla@yahoo.com.br>
2019-05-22 11:06:42 -0300

    Add and configure axios, add propTypes and api call to github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import api from '../../services/api'
 
 import logo from '../../assets/logo.png'
 
@@ -6,15 +7,45 @@ import { Container, Form } from './styles'
 
 import CompareList from '../../components/CompareList'
 
-export default () => (
-  <Container>
-    <img src={logo} alt='Github Compare' />
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  }
 
-    <Form>
-      <input type='text' placeholder='user/repository' />
-      <button>+</button>
-    </Form>
+  handleAddRepository = async (e) => {
+    const { repositoryInput } = this.state
+    e.preventDefault()
 
-    < CompareList />
-  </Container>
-)
+    try {
+      const response = await api.get(`/repos/${repositoryInput}`)
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, response.data]
+      })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    const { repositories, repositoryInput } = this.state
+
+    return(
+      <Container>
+        <img src={logo} alt='Github Compare' />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+           type='text'
+           placeholder='user/repository'
+           value={repositoryInput}
+           onChange={ e => this.setState({ repositoryInput: e.target.value })} />
+          <button type='submit'>ok</button>
+        </Form>
+
+        < CompareList repositories={repositories} />
+      </Container>
+    )
+  }
+}
